eadd87048cbf9875a3ce7764e2a1070651d457cf
Samuel Jauregui <samjaurz_@hotmail.com>
2025-01-15 23:26:44 -0800

    Login page

--- website/views.py
+++ website/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login , logout
+from django.contrib import messages
 
 def home(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('home')
+    else:
+        return render(request, 'home.html')
+
+def logout_user(request):
+    return render(request, 'logged_out.html')
 
